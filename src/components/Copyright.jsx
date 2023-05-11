c88e42412f12aa7101cdb04747cc1c5fcd387b55
Chloe <tran.m.chloe@gmail.com>
2023-05-11 12:55:22 -0400

    Update copyright
    - Replace copyright on Index page with the comp
    - Edit the prop to class instead of page
    - Edit to 2023

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Copyright(props){
     return(
-        <footer id='copyright' className={props.page}>Citron Café 2022</footer>
+        <footer id='copyright' className={props.class}>Citron Café 2023</footer>
     )
 }
 
--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -9,6 +9,7 @@ import SubHero from './SubHero'
 import Info from './Info'
 import Newsletter from './Newsletter'
 import Hero from './Hero'
+import Copyright from '../../components/Copyright'
 
 // import hero slider images 
 import image1 from '../../assets/main-img/img1.jpg'
@@ -18,7 +19,7 @@ import image4 from '../../assets/main-img/img4.jpg'
 import image5 from '../../assets/main-img/img5.jpg'
 import image6 from '../../assets/main-img/img6.jpg'
 
-function IndexPage(props){
+function IndexPage(){
 
     const images = [image1,image2,image3,image4,image5,image1]
     
@@ -33,7 +34,7 @@ function IndexPage(props){
            <Info />
            <Newsletter />
         </Main>
-        <footer id='index-copyright'>Citron Café 2020</footer>
+        <Copyright class="index-copyright"/>
         </>
     )
 }
